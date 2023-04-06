201be2573f177e3435be605a8c7fcc348200deb4
aquino062 <willian_wjunior@hotmail.com>
2023-04-06 17:19:10 -0300

    .

--- exercicios/18-3h.js
+++ exercicios/18-3h.js
@@ -1,2 +1,15 @@
-let numAleatorio = Math.floor(Math.random() * 100 );
-console.log(numAleatorio);
+// Sorteia os valores aleatórios
+let aleatorioMinimo = Math.floor(Math.random() * 100) + 1;
+let aleatorioMaximo = Math.floor(Math.random() * 100) + 1;
+let passoAleatorio = Math.floor(Math.random() * 5) + 1;
+
+// Imprime os valores sorteados
+console.log(`Valor mínimo: ${aleatorioMinimo} / Valor máximo: ${aleatorioMaximo} / Incremento: ${passoAleatorio}`);
+
+// Loop para incrementar o valor mínimo até chegar no valor máximo
+while (aleatorioMinimo < aleatorioMaximo) {
+  aleatorioMinimo += passoAleatorio;
+
+  // Imprime os valores a cada iteração
+  console.log(`Valor mínimo: ${aleatorioMinimo} / Valor máximo: ${aleatorioMaximo} / Incremento: ${passoAleatorio}`);
+}
